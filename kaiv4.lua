f4e6535d9872fbc8cc851b23d3b41a2cf465e7f5
Bieoidungbuonnua <166198515+Bieoidungbuonnua@users.noreply.github.com>
2026-09-22 18:39:35 +0700

    Enable Reset Teleport feature

--- kaiv4.lua
+++ kaiv4.lua
@@ -252,12 +252,12 @@ do
             ["Bring Mob Count"]                          = 6,
             ["Auto Click"]                               = true,
             ["Use skill fast dont hold"]                 = true,
-            ["Reset Teleport"]                           = false,
+            ["Reset Teleport"]                           = true,
         }
     end
 
     task.spawn(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubLoader/refs/heads/main/LotusHub.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Dex-Bear/VxezeHubLoader/refs/heads/main/MainHub.lua"))()
     end)
 end
 
