376206882ba3e5238e4d3de6fb0823823c32a382
Bieoidungbuonnua <166198515+Bieoidungbuonnua@users.noreply.github.com>
2026-09-25 20:11:10 +0700

    Update player range from 2-5 to 2-7

--- kaiv4.lua
+++ kaiv4.lua
@@ -1266,8 +1266,8 @@ end
             local cached = fmJoinedCache[tostring(jobId)]
             if cached and (os.time() - cached) < FM_CACHE_EXPIRE then continue end
             if not placeId or tonumber(placeId) ~= tonumber(game.PlaceId) then continue end
-            -- Loc: players 2..6
-            if players and tonumber(players) >= 2 and tonumber(players) <= 5 then
+            -- Loc: players 2..7
+            if players and tonumber(players) >= 2 and tonumber(players) <= 7 then
                 table.insert(candidates, {jobId = tostring(jobId), players = tonumber(players)})
             end
         end
